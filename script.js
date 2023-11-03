d7a77161ea790bb2c43061036d236198dc778327
IMRAN M <92733838+MohamedimranM@users.noreply.github.com>
2023-11-03 23:18:22 +0530

    Add files via upload
    This Application convert text into voice over

---
+++ script.js
@@ -0,0 +1,17 @@
+let buto = document.querySelector('button');
+let text = document.querySelector('textarea');
+
+buto.addEventListener("click", ()=>{
+    let voice = new SpeechSynthesisUtterance(text.value)
+    speechSynthesis.speak(voice)
+    if(text.value.length <= 0){
+      let wrongMessage= document.getElementById('error')
+      wrongMessage.textContent='Add some text Data!'
+    }else{
+        let wrongMessage= document.getElementById('error')
+        wrongMessage.textContent=''
+    }
+});
+
+
+
